ad06424888051393cda8a35b1cb2f05c7971bf0f
Bastien <record.bastien@gmail.com>
2024-07-18 20:42:35 +0200

    init firebase
    Add firebase dependencies

---
+++ among-us/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAdmCRxxWJTMGYf9RczAUuz_owo4yCFnvU",
+        authDomain: "amongus-friends.firebaseapp.com",
+        projectId: "amongus-friends",
+        storageBucket: "amongus-friends.appspot.com",
+        messagingSenderId: "7874269906",
+        appId: "1:7874269906:web:5d22396dccb1b6e7e5930b"
+    }
+}
